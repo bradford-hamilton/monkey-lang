8bed6dd233ac94003c0c1717440145427cdac8f0
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2019-08-04 16:42:51 -0600

    Modify the AST and it’s nodes to have ability to return the whole program as strings and write simple test for it

--- ast/ast.go
+++ ast/ast.go
@@ -1,10 +1,15 @@
 package ast
 
-import "github.com/bradford-hamilton/monkey-lang/token"
+import (
+	"bytes"
+
+	"github.com/bradford-hamilton/monkey-lang/token"
+)
 
 // Node - nodes in our ast will provide a TokenLiteral method for debugging
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 // Statement - must provide statementNode and TokenLiteral method. Statements do not produce values.
@@ -33,6 +38,17 @@ func (p *RootNode) TokenLiteral() string {
 	return ""
 }
 
+// String returns a buffer containing the programs Statements as strings.
+func (p *RootNode) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 // LetStatement - Name holds the identifier of the binding and Value for the expression
 // that produces the value.
 type LetStatement struct {
@@ -46,7 +62,24 @@ func (ls *LetStatement) statementNode() {}
 // TokenLiteral returns the LetStatement's Literal and satisfies the Node interface.
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
-// Identifier -
+// String - returns a string representation of the LetStatement and satisfies our Node interface
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+// Identifier - holds IDENTIFIER token and it's value (add, foobar, x, y, ...)
 type Identifier struct {
 	Token token.Token // The token.IDENTIFIER token
 	Value string
@@ -57,6 +90,10 @@ func (i *Identifier) expressionNode() {}
 // TokenLiteral returns the Identifier's Literal and satisfies the Node interface.
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 
+// String - returns a string representation of the Identifier and satisfies our Node interface
+func (i *Identifier) String() string { return i.Value }
+
+// ReturnStatement - pretty self explanatory, holds RETURN token and return value
 type ReturnStatement struct {
 	Token       token.Token // The 'return' token
 	ReturnValue Expression
@@ -66,3 +103,38 @@ func (rs *ReturnStatement) statementNode() {}
 
 // TokenLiteral returns the ReturnStatement's Literal and satisfies the Node interface.
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+// String - returns a string representation of the ReturnStatement and satisfies our Node interface
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+// ExpressionStatement - holds the first token of the expression and the expression
+type ExpressionStatement struct {
+	Token      token.Token // The first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+
+// TokenLiteral returns the ExpressionStatement's Literal and satisfies the Node interface.
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+// String - returns a string representation of the ExpressionStatement and satisfies our Node interface
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+
+	return ""
+}
---
+++ ast/ast_test.go
@@ -0,0 +1,29 @@
+package ast
+
+import (
+	"testing"
+
+	"github.com/bradford-hamilton/monkey-lang/token"
+)
+
+func TestString(t *testing.T) {
+	program := &RootNode{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.IDENTIFIER, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.IDENTIFIER, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+
+	if program.String() != "let myVar = anotherVar;" {
+		t.Errorf("program.String() wrong. Got: %q", program.String())
+	}
+}
