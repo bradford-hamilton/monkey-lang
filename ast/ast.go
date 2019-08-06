d89487bbac21acd92e2b301331ec86fcfc1402fd
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2019-08-06 09:11:40 -0600

    Start set up for if expressions and block statements

--- ast/ast.go
+++ ast/ast.go
@@ -219,3 +219,57 @@ func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
 
 // String - returns a string representation of the boolean and satisfies our Node interface
 func (b *Boolean) String() string { return b.Token.Literal }
+
+// BlockStatement - holds the token "{", and a slice of statements
+type BlockStatement struct {
+	Token      token.Token // The { token
+	Statements []Statement
+}
+
+func (bs *BlockStatement) expressionNode() {}
+
+// TokenLiteral returns the BlockStatement's Literal and satisfies the Node interface.
+func (bs *BlockStatement) TokenLiteral() string { return bs.Token.Literal }
+
+// String - returns a string representation of the statements inside the block
+func (bs *BlockStatement) String() string {
+	var out bytes.Buffer
+
+	for _, s := range bs.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
+// IfExpression - holds the token, the condition expression and the consequence & alternative
+// block statements. Structure: if (<condition>) <consequence> else <alternative>
+type IfExpression struct {
+	Token       token.Token // The IF token
+	Condition   Expression
+	Consequence *BlockStatement
+	Alternative *BlockStatement
+}
+
+func (ie *IfExpression) expressionNode() {}
+
+// TokenLiteral returns the IfExpression's Literal and satisfies the Node interface.
+func (ie *IfExpression) TokenLiteral() string { return ie.Token.Literal }
+
+// String - returns a string representation of the IfExpression with the consequence and
+// also the alteritive if it is not nil. Satisfies our Node interface
+func (ie *IfExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("if")
+	out.WriteString(ie.Condition.String())
+	out.WriteString(" ")
+	out.WriteString(ie.Consequence.String())
+
+	if ie.Alternative != nil {
+		out.WriteString("else ")
+		out.WriteString(ie.Alternative.String())
+	}
+
+	return out.String()
+}
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -353,6 +353,104 @@ func TestBooleanExpression(t *testing.T) {
 	}
 }
 
+func TestIFExpression(t *testing.T) {
+	input := `if (x < y) { x }`
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program doesn't contain %d statements. Got: %d", 1, len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not an *ast.ExpressionStatement. Got: %T", program.Statements[0])
+	}
+
+	expr, ok := stmt.Expression.(*ast.IfExpression)
+	if !ok {
+		t.Fatalf("stmt.Expression is not an *ast.IfExpression. Got: %T", stmt.Expression)
+	}
+	if !testInfixExpression(t, expr.Condition, "x", "<", "y") {
+		return
+	}
+	if len(expr.Consequence.Statements) != 1 {
+		t.Errorf("Consequence is not 1 statement. Got: %d", len(expr.Consequence.Statements))
+	}
+
+	consequence, ok := expr.Consequence.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("expr.Consequence.Statements[0] is not an *ast.ExpressionStatement. Got: %T", expr.Consequence.Statements[0])
+	}
+	if !testIdentifier(t, consequence.Expression, "x") {
+		return
+	}
+	if expr.Alternative != nil {
+		t.Errorf("expr.Alternative.Statements was not nil. Got: %+v", expr.Alternative)
+	}
+}
+
+func TestIfElseExpression(t *testing.T) {
+	input := `if (x < y) { x } else { y }`
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program.Statements does not contain %d statements. got=%d\n",
+			1, len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T",
+			program.Statements[0])
+	}
+
+	exp, ok := stmt.Expression.(*ast.IfExpression)
+	if !ok {
+		t.Fatalf("stmt.Expression is not ast.IfExpression. got=%T", stmt.Expression)
+	}
+
+	if !testInfixExpression(t, exp.Condition, "x", "<", "y") {
+		return
+	}
+
+	if len(exp.Consequence.Statements) != 1 {
+		t.Errorf("consequence is not 1 statements. got=%d\n",
+			len(exp.Consequence.Statements))
+	}
+
+	consequence, ok := exp.Consequence.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("Statements[0] is not ast.ExpressionStatement. got=%T",
+			exp.Consequence.Statements[0])
+	}
+
+	if !testIdentifier(t, consequence.Expression, "x") {
+		return
+	}
+
+	if len(exp.Alternative.Statements) != 1 {
+		t.Errorf("exp.Alternative.Statements does not contain 1 statements. got=%d\n",
+			len(exp.Alternative.Statements))
+	}
+
+	alternative, ok := exp.Alternative.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("Statements[0] is not ast.ExpressionStatement. got=%T",
+			exp.Alternative.Statements[0])
+	}
+
+	if !testIdentifier(t, alternative.Expression, "y") {
+		return
+	}
+}
+
 func testIdentifier(t *testing.T, expr ast.Expression, value string) bool {
 	ident, ok := expr.(*ast.Identifier)
 	if !ok {
