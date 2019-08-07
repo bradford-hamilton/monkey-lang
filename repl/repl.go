3fbda7ac1faaab7c31d421afe069e45475467e9d
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2019-08-06 19:50:13 -0600

    Adjust real to print parsed code… and a neat monkey face error message

--- repl/repl.go
+++ repl/repl.go
@@ -6,10 +6,10 @@ import (
 	"io"
 
 	"github.com/bradford-hamilton/monkey-lang/lexer"
-	"github.com/bradford-hamilton/monkey-lang/token"
+	"github.com/bradford-hamilton/monkey-lang/parser"
 )
 
-const prompt = "> "
+const prompt = ">> "
 
 // Start - starts REPL, passes stdin to lexer line by line
 func Start(in io.Reader, out io.Writer) {
@@ -17,17 +17,46 @@ func Start(in io.Reader, out io.Writer) {
 
 	for {
 		fmt.Printf(prompt)
-		scanned := scanner.Scan()
 
+		scanned := scanner.Scan()
 		if !scanned {
 			return
 		}
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for t := l.NextToken(); t.Type != token.EOF; t = l.NextToken() {
-			fmt.Printf("%+v\n", t)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+// MonkeyFace ...need I explain?
+const MonkeyFace = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MonkeyFace)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
