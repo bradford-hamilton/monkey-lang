c2cd131307cad1d41f4576e21f1cabef16560f4e
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2019-08-09 17:25:33 -0600

    add Function struct to monkeys object types

--- object/object.go
+++ object/object.go
@@ -1,6 +1,12 @@
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"strings"
+
+	"github.com/bradford-hamilton/monkey-lang/ast"
+)
 
 type ObjectType string
 
@@ -11,6 +17,7 @@ const (
 	NullObj        = "NULL"
 	ReturnValueObj = "RETURN_VALUE"
 	ErrorObj       = "ERROR"
+	FunctionObj    = "FUNCTION"
 )
 
 // Object represents monkey's object system. Every value in monkey-lang
@@ -94,4 +101,34 @@ func (e *Environment) Get(name string) (Object, bool) {
 func (e *Environment) Set(name string, val Object) Object {
 	e.store[name] = val
 	return val
+}
+
+// Function holds Parameters as a slice of *Identifier, a Body which is a *ast.BlockStatement
+// and a pointer to it's environment
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
+}
+
+// Type returns our Function's ObjectType (FunctionObj)
+func (f *Function) Type() ObjectType { return FunctionObj }
+
+// Inspect returns a string representation of the function definition
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("func")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n")
+
+	return out.String()
 }
