63d3a93b0068da036e65379da801dc2217660f8e
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2019-08-31 16:00:24 -0600

    add tests for hash

--- object/object_test.go
+++ object/object_test.go
@@ -27,6 +27,44 @@ func TestStringHashKey(t *testing.T) {
 	}
 }
 
+func TestBooleanHashKey(t *testing.T) {
+	true1 := &Boolean{Value: true}
+	true2 := &Boolean{Value: true}
+	false1 := &Boolean{Value: false}
+	false2 := &Boolean{Value: false}
+
+	if true1.HashKey() != true2.HashKey() {
+		t.Errorf("booleans with same content have different hash keys")
+	}
+
+	if false1.HashKey() != false2.HashKey() {
+		t.Errorf("booleans with same content have different hash keys")
+	}
+
+	if true1.HashKey() == false1.HashKey() {
+		t.Errorf("booleans with different content have same hash keys")
+	}
+}
+
+func TestIntegerHashKey(t *testing.T) {
+	int1 := &Integer{Value: 10}
+	int2 := &Integer{Value: 10}
+	diff1 := &Integer{Value: 30}
+	diff2 := &Integer{Value: 30}
+
+	if int1.HashKey() != int2.HashKey() {
+		t.Errorf("integers with same content have different hash keys")
+	}
+
+	if diff1.HashKey() != diff2.HashKey() {
+		t.Errorf("integers with same content have different hash keys")
+	}
+
+	if int1.HashKey() == diff1.HashKey() {
+		t.Errorf("integers with different content have same hash keys")
+	}
+}
+
 func TestHash(t *testing.T) {
 	h := &Hash{
 		Pairs: map[HashKey]HashPair{
